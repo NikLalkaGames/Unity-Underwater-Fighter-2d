288818d7dde378268e71dd52732f7bf59039164c
Fara <farhat3027@gmail.com>
2021-04-02 17:15:46 +0300

    Initial commit

---
+++ Assets/Bolchie/Scripts/Demo.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Basic Player Script//
+//controls: 
+//A, D, Left, Right to move
+//Left Alt to attack
+//Space to jump
+//Z is to see dead animation
+
+public class Demo : MonoBehaviour
+{
+    //variable for how fast player runs//
+    [SerializeField] private float hSpeed = 5f;
+
+    [SerializeField] private float vSpeed = 3f;
+
+    private float speed = 5f;
+
+    private bool facingRight = true;
+    private Animator anim;
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    //variable for how high player jumps//
+    [SerializeField]
+    private float jumpForce = 300f;
+
+    public Rigidbody2D rb { get; set; }
+
+    bool dead = false;
+    bool attack = false;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    //movement//
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (!dead && !attack)
+        {
+            anim.SetFloat("vSpeed", rb.velocity.y);
+            anim.SetFloat("Speed", Mathf.Abs(horizontal));
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+
+        if (horizontal > 0 && !facingRight && !dead && !attack)
+        {
+            Flip(horizontal);
+        }
+        else if (horizontal < 0 && facingRight && !dead && !attack)
+        {
+            Flip(horizontal);
+        }
+        if (vertical != 0)
+        {
+
+        }
+    }
+
+    //attacking and jumping//
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftAlt) && !dead)
+        {
+            attack = true;
+            anim.SetBool("Attack", true);
+            anim.SetFloat("Speed", 0);
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            attack = false;
+            anim.SetBool("Attack", false);
+        }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space) && !dead)
+        {
+            anim.SetBool("Ground", false);
+            anim.SetBool("isJumping", true);
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+
+        //dead animation for testing//
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (!dead)
+            {
+                anim.SetBool("Dead", true);
+                anim.SetFloat("Speed", 0);
+                dead = true;
+            }
+            else
+            {
+                anim.SetBool("Dead", false);
+                dead = false;
+            }
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+}
---
+++ Assets/Bolchie/Scripts/PlayerController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // transform and physics variables //
+    [SerializeField] private Vector2 speed =  new Vector2(5f, 3f);
+
+    private Vector2 movement = new Vector3();
+
+    private Vector3 velocity = Vector3.zero;
+    
+    [SerializeField] private float movementSmooth = 0.2f;
+
+
+    float groundRadius = 0.2f;
+
+    public Transform groundCheck;
+
+    private Animator anim;
+
+    private bool facingRight = true;
+
+    
+    [SerializeField] private float jumpForce = 300f;
+
+
+    public LayerMask whatIsGround;
+
+    //variable for how high player jumps//
+
+    public Rigidbody2D rb { get; set; }
+    
+    bool grounded = false;
+
+    bool dead = false;
+
+    bool attack = false;
+
+    bool isJumping = false;
+
+    bool isLanding = false;
+    
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void FixedUpdate() 
+    {
+        HandleMovement();
+    }
+
+    public void HandleMovement()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        if (!dead && !attack)
+        {
+            Vector3 targetVelocity = new Vector3(movement.x * speed.x, movement.y * speed.y);
+
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmooth);
+
+            anim.SetFloat("Speed", Mathf.Abs(movement.magnitude));
+
+            if (movement.x > 0 && !facingRight && !dead && !attack)
+            {
+                Flip(movement.x);
+            }
+            else if (movement.x < 0 && facingRight && !dead && !attack)
+            {
+                Flip(movement.x);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        
+        if (Input.GetKeyDown(KeyCode.LeftAlt) && !dead)
+        {
+            attack = true;
+            anim.SetBool("Attack", true);
+            anim.SetFloat("Speed", 0);
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            attack = false;
+            anim.SetBool("Attack", false);
+        }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space) && !dead)
+        {
+            anim.SetBool("Ground", true);
+            rb.AddForce(Vector3.up * jumpForce * Time.deltaTime, ForceMode2D.Impulse);
+        }
+
+        if (grounded && Input.GetKeyUp(KeyCode.Space)  && !dead)
+        {
+            anim.SetBool("Ground", false);
+        }
+
+        //dead animation for testing//
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (!dead)
+            {
+                anim.SetBool("Dead", true);
+                anim.SetFloat("Speed", 0);
+                dead = true;
+            }
+        }
+        
+        if (dead)
+        {
+            anim.SetBool("Dead", false);
+            dead = false;
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+}
