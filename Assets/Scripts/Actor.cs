965ae2a81db08563bb12e9df68a39fa7e6a8f4f1
Orpheus424 <68345234+Orpheus424@users.noreply.github.com>
2021-04-02 23:08:00 +0300

    Add PlayerController

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Actor : MonoBehaviour {
+    public Animator baseAnim;
+    public Rigidbody body;
+    protected bool canFlinch = true;
+    public float speed = 2;
+    public bool isAlive = true;
+    public float maxLife = 100.0f;
+    public float currentLife = 100.0f;
+    protected Vector3 frontVector;
+    protected virtual void Start()
+    {
+        currentLife = maxLife;
+        isAlive = true;
+        baseAnim.SetBool("IsAlive", isAlive);
+    }
+
+    public virtual void Update()
+    {
+
+    }
+
+    public void FlipSprite(bool isFacingLeft)
+    {
+        if (isFacingLeft)
+        {
+            frontVector = new Vector3(-1, 0, 0);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            frontVector = new Vector3(1, 0, 0);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Actor
+{
+    Vector3 curDirection;
+    bool isFacingLeft;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+
+    public override void Update()
+    {
+        //calls superclass
+        base.Update();
+
+        //handles button presses after death
+        if (!isAlive)
+            return;
+
+
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        curDirection = new Vector3(h, 0, v);
+        curDirection.Normalize();
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        //prevents movements after death
+        if (!isAlive)
+            return;
+
+
+        Vector3 moveVector = curDirection * speed;
+        body.MovePosition(transform.position + moveVector * Time.fixedDeltaTime);
+        baseAnim.SetFloat("Speed", moveVector.magnitude);
+
+        //flips sprite towards movement direction
+
+        if (moveVector.x != 0)
+            isFacingLeft = moveVector.x < 0;
+        FlipSprite(isFacingLeft);
+            
+        
+    }
+        
+}
