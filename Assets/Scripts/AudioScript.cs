9e08743d0ea646858d5c09316cae6bbb2f54b90c
CaShNear <35704985+CaShNear@users.noreply.github.com>
2021-04-04 19:05:58 +0300

    Added music with some pain in the ass

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    private static AudioScript instance = null;
+      private AudioSource audioMusic;
+
+      private void Awake()
+      {
+          if (instance == null)
+          { 
+               instance = this;
+               DontDestroyOnLoad(gameObject);
+               return;
+          }
+          if (instance == this) return; 
+          Destroy(gameObject);
+      }
+
+      void Start()
+      {
+         audioMusic = GetComponent<AudioSource>();
+         audioMusic.loop = true;
+         audioMusic.Play();
+      }
+}
