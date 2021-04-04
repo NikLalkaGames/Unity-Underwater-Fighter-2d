380671cc3dbb228888a9d2d0962e0535413c740c
CaShNear <35704985+CaShNear@users.noreply.github.com>
2021-04-04 16:52:45 +0300

    Added Game Over Canvas without turning on the canvas after death

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -3,33 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenuScript : MonoBehaviour
+public class GameOverScript : MonoBehaviour
 {
-    public static bool gameIsPaused = false;
-    public GameObject pauseMenuUI;
+
+    public GameObject gameOverMenuUI;
 
     // Update is called once per frame
-    void Update()
+    void FixUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)){
-            if (gameIsPaused){
-                Resume();
-            } else {
-                Pause();
-            }
+        /* Короче, здесь наверное надо вставить условие включения GameOverMenu
+        
+        if (хп_нашего_Булча < 0){
+            gameOverMenuUI.SetActive(true);
+            Time.timeScale = 0f;
         }
-    }
-
-    public void Resume(){
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        gameIsPaused = false;
-    }
-
-    void Pause(){
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        gameIsPaused = true;
+        */
     }
 
     public void GoToMenu(){
